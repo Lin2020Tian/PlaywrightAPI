490b3a0553772210a91a5788977254fcc21a2d0f
LinTian <lin2020tian@gmail.com>
2024-04-16 12:01:17 +1200

    API test to a create a bug

---
+++ tests/test-gitHub-API.spec.ts
@@ -0,0 +1,26 @@
+import { test, expect } from '@playwright/test';
+import { request } from 'http';
+
+/** test GitHub APIs */
+// set the test repo name and the GitHub username (run command if not known: git config user.name)
+const REPO = 'PlaywrightAPI';
+const USER = 'Lin2020Tian';
+
+// test API of creating Issue in GitHub repo
+test('Create a bug report', async({ request }) => {
+    const bugTitle = '[Bug] report 1';
+    const bugDescription = 'Bug description';
+
+    // create a new issue
+    const newIssue = await request.post('/repos/${USER}/${REPO}/issues', {
+        data: { title: bugTitle, body: bugDescription, }
+    })
+    expect(newIssue.ok()).toBeTruthy();
+
+    // get the newly create issue
+    const issues = await request.get('/repos/${USER}/${REPO}/issues');
+    expect(issues.ok()).toBeTruthy();
+    expect(await issues.json()).toContainEqual(expect.objectContaining({
+        title: bugTitle, body:bugDescription
+    }));
+});
